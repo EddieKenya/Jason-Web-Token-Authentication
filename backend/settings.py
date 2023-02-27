74e7db2c9f06d7815e9fbad38b0d02f2417d906e
Edwin Makhanu <edwinmakhanu630@gmail.com>
2023-02-27 15:15:46 +0300

    cors added

--- backend/settings.py
+++ backend/settings.py
@@ -177,7 +177,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://shimmering-swan-95e0c2.netlify.app/"
 ]
 
 REST_FRAMEWORK = {
